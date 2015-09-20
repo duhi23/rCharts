59ba2d0f8fb56cb3601448a379e9da947755560b
duhi23 <diego.21.phs@hotmail.com>
2015-09-19 23:24:29 -0500

    GoogleVis

---
+++ pages_web.R
@@ -0,0 +1,8 @@
+##############################################
+#### Creacion de paginas web interactivas ####
+##############################################
+
+library(knitr)
+install.packages('googleVis', dependencies = TRUE)
+library(googleVis)
+library(rCharts)
